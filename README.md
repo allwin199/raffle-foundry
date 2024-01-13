6c6ae61169f23613f144d396972ecdc198cd3b52
worksofallwin <princeallwin199@gmail.com>
2024-01-13 14:38:56 +0530

    bump forge version to 1.6.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Proveably Raffle Contract
+
+```sh
+forge script script/DeployRaffle.s.sol:DeployRaffle --rpc-url $SEPOLIA_RPC_URL --account $ACCOUNT_FOR_SEPOLIA --sender $SEPOLIA_KEYCHAIN --broadcast --verify $ETHERSCAN_API_KEY
+```
+
+```sh
+forge script script/DeployRaffle.s.sol:DeployRaffle --rpc-url $ANVIL_RPC_URL --account $ACCOUNT_FOR_ANVIL --sender $ANVIL_KEYCHAIN --broadcast
+```
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -61,7 +61,7 @@ contract HelperConfig is Script {
             entranceFee: ENTRANCE_FEE,
             interval: INTERVAL,
             vrfCoordinatorAddress: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
-            subscriptionId: 0, // If subId is 0 subId will be programatically generated
+            subscriptionId: 0, // If subId is 0 subId will be programatically generated // 8520
             gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
             callbackGasLimit: 500000, //500,000 gas
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
